a7ca9a0f320c6fff824548bf0066d9e785529a8b
Justin <showin@live.com>
2020-02-14 11:25:56 -0700

    sprint upload

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -16,8 +14,20 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # insert ticket key, values into hastable
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    # index first value of route with value associated with NONE key
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+
+    # loop through the rest of the cities and retrieve associated key, values
+    # Use previous destintation as lookup for next destination
+    for index in range(1, length):
+        
+        route[index] = hash_table_retrieve(hashtable, route[index-1])
+    
+    # return final route without None values
+    return route[0:length-1]
+
+    
